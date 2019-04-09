83ce029fa55d92b76801caba4be512a05523b2d8
freshteapot <chris@freshteapot.net>
2019-04-09 17:35:31 +0200

    Better handling of landscape.

--- lib/play/v1/total_recall/info.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class TotalRecallV1Info extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text('I am the info');
-  }
-}
