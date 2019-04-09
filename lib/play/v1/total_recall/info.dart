9455c997cbd3f3acde146360dbadc31b7eeeaac8
freshteapot <chris@freshteapot.net>
2019-04-09 16:16:57 +0200

    Creating total recall for list type v1 (#2).
    - Added a menu for one or more ways to play.
    - Added total recall.

---
+++ lib/play/v1/total_recall/info.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TotalRecallV1Info extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('I am the info');
+  }
+}
